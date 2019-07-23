e665ed95d9f0d90a1f50734f1d0c0eb53278c8f9
Nicole Dow <nikki.dow71@gmail.com>
2019-07-23 18:41:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,8 +60,8 @@ class Song
   def self.new_from_filename(filename)
     components = filename.split(" - ")
     song = self.new
-    song.name = components[0]
-    song.artist_name = components[1].chomp(".mp3")
+    song.artist_name = components[0]
+    song.name = components[1].chomp(".mp3")
   end
   
 end
